ef2b6e3d4c198f7df0bb142d7325cbed28829920
jackedonps <singhpankaj7057@gmail.com>
2022-10-10 18:42:02 +0530

    10octrenew

--- Leetcode6.py
+++ Leetcode6.py
@@ -1,18 +1,16 @@
- 
 from typing import List
 
 
 class Solution:
     def log(self,logs:List[str])->List[str]:
-        res1=[]
-        res2=[]
+        res1,res2=[],[]
         for log in logs:
             if (log.split()[1]).isdigit():
                 res2.append(log)
             else:
                 res1.append(log.split())
-        res1.sort(key=lambda x:x[0])
-        res1.sort(key=lambda x:x[1:])
+        res1.sort(key= lambda x:x[0])
+        res1.sort(key= lambda x:x[1:])
 
         for i in range(len(res1)):
             res1[i]=(" ".join(res1[i]))
